922a1c75dfce7b376a1d9a55b76fc7a52f4850f2
RonanLeBarh <50013468+RonanLeBarh@users.noreply.github.com>
2025-09-20 15:48:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ import {
 
 // ----- Remplace par ta configuration Firebase -----
 const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.appspot.com",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyDJwHqJdmUKayrxTnHrcGHcCLCm9lmOtMY",
+  authDomain: "idleclick-5fc91.firebaseapp.com",
+  projectId: "idleclick-5fc91",
+  storageBucket: "idleclick-5fc91.firebasestorage.app",
+  messagingSenderId: "166564180674",
+  appId: "1:166564180674:web:75dce2c2eb0cc6556fc15b"
 };
 
 // Initialisation conditionnelle (permet d'héberger même sans config remplie)
