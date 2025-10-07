b1e275415ccf9da50d0573b8cdb195fa683a31bf
RonanLeBarh <50013468+RonanLeBarh@users.noreply.github.com>
2025-10-07 19:05:42 +0200

    Updated .gitignore

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDJwHqJdmUKayrxTnHrcGHcCLCm9lmOtMY",
-  authDomain: "idleclick-5fc91.firebaseapp.com",
-  projectId: "idleclick-5fc91",
-  storageBucket: "idleclick-5fc91.firebasestorage.app",
-  messagingSenderId: "166564180674",
-  appId: "1:166564180674:web:75dce2c2eb0cc6556fc15b"
-};
